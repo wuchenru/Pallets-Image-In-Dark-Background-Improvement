bebfe2a1cdb1e9c28408e8bfdd99ac569862f3d2
wuchenru <chenruwu31@gmail.com>
2023-11-08 21:19:37 -0800

    Add some test cases

--- lowlight_test.py
+++ lowlight_test.py
@@ -31,7 +31,7 @@ def lowlight(image_path):
 	data_lowlight = data_lowlight.cuda().unsqueeze(0)
 
 	DCE_net = model.enhance_net_nopool().cuda()
-	DCE_net.load_state_dict(torch.load('snapshots/Epoch99.pth'))
+	DCE_net.load_state_dict(torch.load('trained_weights/Epoch99.pth'))
 	start = time.time()
 	_,enhanced_image,_ = DCE_net(data_lowlight)
 
